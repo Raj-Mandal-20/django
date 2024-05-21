5656bb18919277b691eb357bd9f96a54dc474d2c
Raj-Mandal-20 <usingrajcseinfo@gmail.com>
2024-05-21 21:17:53 +0530

    added remove punctuation functionality

--- module1/practice/views.py
+++ module1/practice/views.py
@@ -7,26 +7,40 @@ from django.shortcuts import render
 #     return HttpResponse("<H1> Home Page </h1> <br> <a href='https://www.youtube.com/watch?v=5BDgKJFZMl8&list=PLu0W_9lII9ah7DDtYtflgwMwpT3xmjXY9'> Django PlayList</a> <br> <a href='https://www.flipkart.com/'> Flipkart</a> <br> <a href='https://www.facebook.com/'> Facebook </a> <br> <a href='https://www.amazon.in/'> amazon</a>  <br> <a href='https://codeforces.com/'> Codeforces</a>")
 
 #Practice For Video no 7
+
+def isPunctuation(item):
+    # checked for alphabets
+    punctuation = '''!"#$%&'()*+, -./:;<=>?@[\]^_`{|}~'''
+    for i in range(len(punctuation)):
+        if(punctuation[i] == item) :
+            return True
+        
+    return False
+
 def index(request):
     params = {'name' : 'Raj', 'place' : 'India'}
     return render(request, 'index.html', params)
     
-
 def about(request):
-    print(request.GET.get('typedData', 'default'))
-    return render(request, 'about.html')
-
-def menu(request):
-    return HttpResponse('''
-        <h1> Menu </h1> <br>
-        <a href="/"> Home </a>
-                        ''')
-
-def settings(requsest):
-    return HttpResponse('''
-        <h1> Settings </h1> <br>
-        <a href="/"> Home </a>
-                        ''')
+    typedData = request.GET.get('typedData', 'default')
+    isChecked = request.GET.get('isChecked', 'off')
     
+    params = {
+        'motivation' : 'Remove Punctuation',
+        'before' : typedData,
+        'after' : typedData
+    }
+    
+    if(isChecked == 'on') :
+        # params['after'] = 'modified'
+        removedPunc = ""
+        for i in range(len(typedData)):
+            if isPunctuation(typedData[i]) == False :
+                removedPunc += typedData[i]
+        
+        params['after'] = removedPunc    
+        
+    return render(request, 'about.html', params)
+
 def textAnalyze(request):
     return render(request, 'textanalyze.html')
